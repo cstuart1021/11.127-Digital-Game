cc19e3b5884f4cc7f663e9ab712209b0a511bdc3
kchadha <kchadha@wellesley.edu>
2013-05-09 01:18:14 -0400

    added level 3

--- src/Model.java
+++ src/Model.java
@@ -5,31 +5,34 @@ import org.newdawn.slick.geom.*;
 
 
 public class Model {
-	
+
 	Screen cur_screen;
 	Progress cur_prog;
 	String cur_error;
-	
+
 
 	StartScreen start_screen;
-	
+
 	Level cur_level;
-	
+
 	//Levels
 	Level1 level1;
-  SandboxLevel sandbox_level;
-  Level2Materials level2;
-	
+	SandboxLevel sandbox_level;
+	Level2Materials level2;
+	Level3 level3;
+
 	//current level
 
-	
+
 	//CommandBoxes
 	ArrayList<CommandBox> boxes = new ArrayList<CommandBox>();
 	String error_1;
 	String error_2;
 	String error_3;
 	
-	
+	String level3_error;
+
+
 	// run button
 	Color run_button_color;
 	Color run_color;
@@ -37,10 +40,10 @@ public class Model {
 	Color next_color;
 	Color back_button_color;
 	Color back_color;
- 
+
 	Rectangle run;
-  Rectangle next;
-  Rectangle back;
+	Rectangle next;
+	Rectangle back;
 	boolean over_run_1;
 	boolean over_next_1;
 	boolean over_back_1;
@@ -52,22 +55,25 @@ public class Model {
 		WORK, ERROR, SUCCESS
 	}
 
-	
+
 	public Model(GameContainer gc) {
-		
+
 		cur_screen = Screen.START;
 		start_screen = new StartScreen();
 		level1 = new Level1(this, gc);
 		sandbox_level = new SandboxLevel(this, gc);
 		level2 = new Level2Materials(this);
+		level3 = new Level3(this,gc);
 		cur_level = start_screen;
-		start_screen.setNextLevel( level1);
+		start_screen.setNextLevel(level1);
 		level1.setPrevLevel(start_screen);
 		level1.setNextLevel(level2);
 		level2.setPrevLevel(level1);
-		level2.setNextLevel(sandbox_level);
-		sandbox_level.setPrevLevel( level2);
-		
+		level2.setNextLevel(level3);
+		level3.setPrevLevel(level2);
+		level3.setNextLevel(sandbox_level);
+		sandbox_level.setPrevLevel(level3);
+
 		run = new Rectangle( 400,400, 100, 100);
 		run_button_color = new Color(0 , 204, 102);
 		run_color = new Color(0, 204, 102);
@@ -81,38 +87,39 @@ public class Model {
 		error_1 = "You do still have some messy things!";
 		error_2 = " no soapy hot water!!";
 		error_3 = " stay on task!";
-    next = new Rectangle( 500,500,100,50);
-    back = new Rectangle(10, 540, 50, 50);
+		level3_error = "Looks like something went wrong!";
+		next = new Rectangle( 500,500,100,50);
+		back = new Rectangle(10, 540, 50, 50);
 
-		
+
 	}
 	public void modifyStack() {
-		
-		
+
+
 	}
-	
+
 	public void modifyCBPos(CommandBox temp, int x, int y) {
-		
+
 		temp.cur_x = x;
 		temp.cur_y = y;
-		
+
 	}
 	public void changeCBColor(Color color, CommandBox temp) {
 		temp.rect_color = color;
 	}
 
-	
+
 	public void run() {
-		
+
 		/*
 		if (cur_screen == Screen.LEVEL1){
 			level1.run();
 		} if (cur_screen == Screen.SANDBOX) {
       sandbox_level.run();
     }
-    */
+		 */
 		cur_level.run();
-		
+
 	}
-	
+
 }
--- src/View.java
+++ src/View.java
@@ -8,24 +8,24 @@ import java.util.ArrayList;
 
 
 public class View {
-	
+
 	Model model;
 	Font times;
 	TrueTypeFont times2;
-	
+
 	public View(Model model) {
-		
+
 		this.model = model;
 		times = new Font("Times New Roman",Font.BOLD, 15);
 		times2 = new TrueTypeFont(times, false);
-		
+
 	}
 	// albhalbab
 
 	// get values from model and draw
 	public void render(GameContainer gc, Graphics g) {
 		g.setFont( times2);
-		
+
 		//start screen
 		if (model.cur_level == model.start_screen){
 			model.start_screen.render(g);
@@ -34,46 +34,49 @@ public class View {
 
 		else {
 			g.setBackground(Color.black);
-      // draw boxes
+			// draw boxes
 			ArrayList<CommandBox> boxes = model.cur_level.getBoxes();
 			for (int x = 0; x < boxes.size(); x++) {
-				
+
 				CommandBox temp_box = boxes.get(x);
 				g.setColor(temp_box.rect_color);
 				temp_box.render();
 				g.setColor(temp_box.text_color);
 				g.drawString(temp_box.str, temp_box.cur_x, temp_box.cur_y);
 			}
-      // draw run button
+			// draw run button
 			g.setColor(model.run_button_color);
 			ShapeRenderer.fill(model.run);
-      g.setColor(Color.black);
+			g.setColor(Color.black);
 			g.drawString("RUN", model.run.getCenterX()-15, model.run.getCenterY() - 5);
-      // draw next
-      if (model.cur_prog == Model.Progress.SUCCESS) {
-        g.setColor(model.next_button_color);
-        ShapeRenderer.fill(model.next);
-        g.setColor(Color.black);
-        g.drawString("Next", model.next.getCenterX() - 15, model.next.getCenterY() - 5);
+			// draw next
+			if (model.cur_prog == Model.Progress.SUCCESS) {
+				g.setColor(model.next_button_color);
+				ShapeRenderer.fill(model.next);
+				g.setColor(Color.black);
+				g.drawString("Next", model.next.getCenterX() - 15, model.next.getCenterY() - 5);
 
-      }
-      //draw back
-      g.setColor(model.back_button_color);
-      ShapeRenderer.fill(model.back);
-      g.setColor(Color.black);
-      g.drawString("Back", model.back.getCenterX() - 15, model.back.getCenterY() - 5);
-      // draw stack 
-      g.setColor(Color.white);
-			model.level1.stack.render(g);
-		  // draw error message	
+			}
+			//draw back
+			g.setColor(model.back_button_color);
+			ShapeRenderer.fill(model.back);
+			g.setColor(Color.black);
+			g.drawString("Back", model.back.getCenterX() - 15, model.back.getCenterY() - 5);
+			// draw stack 
+			g.setColor(Color.white);
+			if(model.cur_level == model.level3){
+				model.level3.stack.render(g);
+			}else
+				model.level1.stack.render(g);
+			// draw error message	
 			if (model.cur_prog == Model.Progress.ERROR){
 				g.setColor(Color.red);
-				g.drawString(model.cur_error, 100, 140);
+				g.drawString(model.cur_error, 140, model.cur_level.getTF().size()*20+40);
 			} else if (model.cur_prog == Model.Progress.SUCCESS) {
 				g.setColor(Color.green);
-				g.drawString(model.cur_error, 100, 140);
+				g.drawString(model.cur_error, 140, model.cur_level.getTF().size()*20+40);
 			}
-      // draw paragraph
+			// draw paragraph
 			ArrayList<TextField> tf_list = model.cur_level.getTF();
 			for (int x = 0; x<tf_list.size(); x++){
 				TextField tf = tf_list.get(x);
@@ -83,10 +86,10 @@ public class View {
 				tf.render(gc, g);
 			}
 
-			
+
 		}
-		
-		
+
+
 	}
 
 }
